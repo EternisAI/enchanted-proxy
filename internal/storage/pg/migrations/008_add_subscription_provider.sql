73978e76a91f89abce2b1876a417a02ef266629f
Marko Lihter <lihter.marko@gmail.com>
2025-12-02 19:58:29 +0100

    stripe: db migration delete

--- internal/storage/pg/migrations/008_add_subscription_provider.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-ALTER TABLE entitlements
-ADD COLUMN IF NOT EXISTS subscription_provider TEXT NULL;
-
-COMMENT ON COLUMN entitlements.subscription_provider IS 'Subscription source: apple, stripe, or NULL for free tier';
-
--- +goose Down
-ALTER TABLE entitlements
-DROP COLUMN IF EXISTS subscription_provider;
--- internal/storage/pg/migrations/009_add_stripe_customer_id.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
-ALTER TABLE entitlements
-ADD COLUMN IF NOT EXISTS stripe_customer_id TEXT NULL;
-
-COMMENT ON COLUMN entitlements.stripe_customer_id IS 'Stripe Customer ID for billing portal access (cus_xxx)';
-
--- +goose Down
-ALTER TABLE entitlements
-DROP COLUMN IF EXISTS stripe_customer_id;
